1e64c43349f4cf7981fc797c88ad00f07dcf97a1
Vipul-Singh11 <itsvipulsingh11@gmail.com>
2026-05-02 00:03:28 +0530

    Refactor OrderServiceClient formatting and imports

--- src/main/java/com/stock/matching_engine_service/client/OrderServiceClient.java
+++ src/main/java/com/stock/matching_engine_service/client/OrderServiceClient.java
@@ -1,6 +1,5 @@
 package com.stock.matching_engine_service.client;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
